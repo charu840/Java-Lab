f04f8cbe74a95686bfc5634601f92bbe9022f1b2
charu840 <saraogicharu@gmail.com>
2026-04-01 11:11:03 +0530

    Create CalculatorOOP.java
    This class provides methods for basic arithmetic operations including addition, subtraction, multiplication, and division, while handling division by zero.

---
+++ CalculatorOOP.java
@@ -0,0 +1,38 @@
+class CalculatorOOP {
+    int a, b;
+
+    // Constructor
+    CalculatorOOP(int x, int y) {
+        a = x;
+        b = y;
+    }
+
+    void add() {
+        System.out.println("Addition: " + (a + b));
+    }
+
+    void sub() {
+        System.out.println("Subtraction: " + (a - b));
+    }
+
+    void mul() {
+        System.out.println("Multiplication: " + (a * b));
+    }
+
+    void div() {
+        if (b != 0)
+            System.out.println("Division: " + (a / b));
+        else
+            System.out.println("Division by zero not possible");
+    }
+
+    public static void main(String[] args) {
+        // Creating object
+        CalculatorOOP obj = new CalculatorOOP(20, 5);
+
+        obj.add();
+        obj.sub();
+        obj.mul();
+        obj.div();
+    }
+}
